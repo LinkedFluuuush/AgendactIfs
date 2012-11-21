05e7b78702a16a5967f2c835c202b0d17ad31b34
Steven <bankai14130@gmail.com>
2012-11-21 16:33:42 +0100

    Reset

--- index.php
+++ index.php
@@ -15,6 +15,9 @@ else
 	$jour = date('d');
 }
 ?>
+
+<!-- Projet événementiel informatisé - Réalisation :  Guillaume ANNE et Julien DIESNIS (2008-2009) --> 
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 "http://www.w3.org/TR/html4/loose.dtd">
 <html>
