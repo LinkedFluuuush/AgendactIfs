3026bf371202b3577196d31388445c2733b1cf11
Cindy Perat <cindy.perat@live.fr>
2013-01-10 17:57:40 +0100

    Ajout liste priorités

--- Pages/menu.php
+++ Pages/menu.php
@@ -11,14 +11,23 @@
                 </form>
             </ul>
         </li>
-         <li>
-             <div class="header">Evénement</div>
-             <ul class="menu">
-                 <li onclick ="document.location.href ='..\\Evenement\\creer.php'"><a href="..\Evenement\creer.php">Ajouter</a></li>
-                 <li onclick ="document.location.href ='#'"><a href="#">Rechercher</a></li>
-             </ul>
-         </li>
-    </ul>
+        <li>
+            <div class="header">Evénement</div>
+            <ul class="menu">
+                <li onclick ="document.location.href ='..\\Evenement\\creer.php'"><a href="..\Evenement\creer.php">Créer</a></li>
+                <li onclick ="document.location.href ='#'"><a href="#">Gérer</a></li>
+                <li onclick ="document.location.href ='#'"><a href="#">Rechercher</a></li>
+            </ul>
+        </li>
+        <form>
+            <select name="changement_priorite">
+                <option value="1">Haute</option>
+                <option value="2">Moyennne</option>
+                <option value="3">Basse</option>
+            </select>
+            <input type="submit" name="valider" value="Valider">
+        </form>
+    </ul><br>
     
     <!-- mini-calendrier -->
      <div id="miniCalendrier">
