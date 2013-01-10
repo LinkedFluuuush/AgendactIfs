239f8b5796cd6b1d68cb72f87ccd4fe584abbce7
System User <jeanbaptiste.louvet@info102.iutc3.unicaen.fr>
2013-01-10 14:18:34 +0100

    Correction du bug sur les groupes

--- Fonctions_Php/XMLSelectEvent.php
+++ Fonctions_Php/XMLSelectEvent.php
@@ -2,7 +2,7 @@
 include("./connexion.php");
 
 //Récupération groupe
-$sql = "SELECT idgroupe_1, libelle FROM aci_bdd.aci_contenir JOIN aci_groupe ON (aci_groupe.idgroupe = aci_contenir.idgroupe_1) where aci_contenir.idgroupe = ".$_POST['valeur'].";";
+$sql = "SELECT idgroupe_1, libelle FROM aci_contenir JOIN aci_groupe ON (aci_groupe.idgroupe = aci_contenir.idgroupe_1) where aci_contenir.idgroupe = ".$_POST['valeur'].";";
 		
 $resultats = $conn->query($sql);
 while($row = $resultats->fetch())
