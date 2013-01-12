102663865850bd9deb4bfcf4ca0fcd63e5ff906f
Cindy Perat <cindy.perat@live.fr>
2013-01-12 17:25:42 +0100

    suppression ancien fichiers de connexion Connexion/formConnexion.php Connexion/traitementConnexion.php

--- Pages/Connexion/formConnexion.php
+++
@@ -1,5 +0,0 @@
-<form method=post name="formConnexion" action="../Connexion/traitementConnexion.php" >
-    Nom d'utilisateur :<br><input type="text" name="nomUtilisateur"><br>
-    Mot de passe :<br><input type="password" name="motDePasse"><br>
-    <input type="submit">
-</form>
