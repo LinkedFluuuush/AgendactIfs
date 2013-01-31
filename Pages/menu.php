42b52b92532a2297c6fc043d3f1a81b1672bc639
Cindy Perat <cindy.perat@live.fr>
2013-01-31 15:28:38 +0100

    correction menu

--- Pages/menu.php
+++ Pages/menu.php
@@ -8,7 +8,7 @@
                 <li>
                     <?php if(empty($_SESSION['id'])){ 
                         if(isset($_GET['login']) && $_GET['login'] == 0){
-                            echo "<span style=\"color:red\">Connexion echouée</span>";
+                            echo "<span style=\"color:red\">Connexion echouée.</span>";
                         } ?>
                     <form name="connexion" action="../../Fonctions_Php/connexionLDAP.php" method="POST">
                         <input type="text" name="login" placeholder="Identifiant"><br>
@@ -30,28 +30,37 @@
                 <li onclick ="document.location.href ='#'"><a href="#">Rechercher</a></li>-->
             </ul>
         </li>
-        <?php 
-        $temp = explode("/", $_SERVER['PHP_SELF']);
-        $nomPage = $temp[sizeof($temp)-1];
-
-        if($nomPage == "semestre.php" || $nomPage == "mois.php" ||$nomPage == "semaine.php" ||$nomPage == "jour.php")
-            include("priorite.php"); ?>
     </ul>
     
-    <!-- mini-calendrier -->
-     <div id="miniCalendrier">
-        <script type="text/javascript">miniCalendrier(<?php echo "$annee, $mois-1"; ?>);</script>
-     </div>
-    <br>
     <ul class="nav">
         <li>
             <div class="header">Vue</div>
             <ul class="menu">
-                <li onclick ="document.location.href ='..\\Calendrier\\semestre.php'"><a href="..\Calendrier\semestre.php">Semestre</a></li>
-                <li onclick ="document.location.href ='..\\Calendrier\\mois.php'"><a href="..\Calendrier\mois.php">Mois</a></li>
-                <li onclick ="document.location.href ='..\\Calendrier\\semaine.php'"><a href="..\Calendrier\semaine.php">Semaine</a></li>
-                <li onclick ="document.location.href ='..\\Calendrier\\jour.php'"><a href="..\Calendrier\jour.php">Jour</a></li>
+                <li onclick ="document.location.href ='../Calendrier/semestre.php'"><a href="../Calendrier/semestre.php">Semestre</a></li>
+                <li onclick ="document.location.href ='../Calendrier/mois.php'"><a href="../Calendrier/mois.php">Mois</a></li>
+                <li onclick ="document.location.href ='../Calendrier/semaine.php'"><a href="../Calendrier/semaine.php">Semaine</a></li>
+                <li onclick ="document.location.href ='../Calendrier/jour.php'"><a href="../Calendrier/jour.php">Jour</a></li>
             </ul>
         </li>
      </ul>
+    
+    <ul class="nav">
+        <?php 
+        $temp = explode("/", $_SERVER['PHP_SELF']);
+        $nomPage = $temp[sizeof($temp)-1];
+
+        if($nomPage == "semestre.php" or $nomPage == "mois.php" or $nomPage == "semaine.php" or $nomPage == "jour.php")
+            include("priorite.php"); ?>
+    </ul>
+    
+    <?php
+    $temp = explode("/", $_SERVER['PHP_SELF']);
+    $nomPage = $temp[sizeof($temp)-1];
+    
+    if ($nomPage == "mois.php" or $nomPage == "semaine.php" or $nomPage == "jour.php") { ?>
+        <!-- mini-calendrier -->
+        <div id="miniCalendrier">
+           <script type="text/javascript">miniCalendrier(<?php echo "$annee, $mois-1"; ?>);</script>
+        </div>
+    <?php } ?>    
 </div>
