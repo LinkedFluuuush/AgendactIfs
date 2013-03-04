544bd90de65c666a702670e77fd4a5c95fa4ed54
LinkedFluuuush <linkedfluuuush@gmail.com>
2013-03-04 17:55:08 +0100

    Recherche par le prénom opérationelle

--- Fonctions_Php/XMLgetPersonne.php
+++ Fonctions_Php/XMLgetPersonne.php
@@ -3,7 +3,7 @@ include("./connexion.php");
 
 $str = "";
 
-$sql="SELECT CONCAT(nom,' ', prenom,' ', adresse_mail) AS info FROM aci_utilisateur WHERE CONCAT(nom,' ', prenom) like '".strtoupper($_POST['valeur'])."%' ORDER BY nom, prenom LIMIT 10 ;";
+$sql="SELECT CONCAT(nom,' ', prenom,' ', adresse_mail) AS info FROM aci_utilisateur WHERE CONCAT(nom,' ', prenom) like '".strtoupper($_POST['valeur'])."%' OR CONCAT(prenom,' ',nom) like '".strtoupper($_POST['valeur'])."%' ORDER BY nom, prenom LIMIT 10 ;";
 $resultats = $conn->query($sql);
 while($row = $resultats->fetch())
     $str .= utf8_encode($row['info']).'|';
