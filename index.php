7d5c9fa394727ff5766d0adaabc4003648f49508
Cindy Perat <cindy.perat@live.fr>
2013-03-13 19:20:43 +0100

    changement nom de la page index.php commentaires dans les fichiers css

--- index.php
+++ index.php
@@ -1,32 +1,30 @@
 <?php
-
 session_start();
 
-if (!empty($_SESSION['annee']) && !empty($_SESSION['mois']) && !empty($_SESSION['jour']))
-{
-	$annee = $_SESSION['annee'];
-	$mois = $_SESSION['mois'];
-	$jour = $_SESSION['jour'];
+if (!empty($_SESSION['annee']) && !empty($_SESSION['mois']) && !empty($_SESSION['jour'])) {
+    $annee = $_SESSION['annee'];
+    $mois = $_SESSION['mois'];
+    $jour = $_SESSION['jour'];
 }
-else
-{
-	$annee = date('Y');
-	$mois = date('m');
-	$jour = date('d');
+else {
+    $annee = date('Y');
+    $mois = date('m');
+    $jour = date('d');
 }
 ?>
 
-<!-- Projet événementiel informatisé - Réalisation :  Guillaume ANNE et Julien DIESNIS (2008-2009) --> 
-
 <!DOCTYPE HTML>
 <html>
-<head>
-	<title>Calendrier Evénementiel : Ifs Campus III</title>
-	<meta HTTP-EQUIV="content-type" CONTENT="text/html; charset=ISO-8859-1">
-</head>
+    <head>
+        <meta HTTP-EQUIV="content-type" CONTENT="text/html; charset=UTF-8">
+        <title>Agendact'Ifs</title>
+    </head>
 
-<body>
-<script><?php echo('document.location.href =\'Pages/Calendrier/mois.php?a='.$annee.'&m='.$mois.'\''); ?></script>
-</body>
+    <body>
+        <script>
+            // Redirection de la page d'index vers la page mois
+            <?php echo('document.location.href =\'Pages/Calendrier/mois.php?a='.$annee.'&m='.$mois.'\''); ?>
+        </script>
+    </body>
 </html>
 
